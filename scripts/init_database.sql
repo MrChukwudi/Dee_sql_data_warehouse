d4cde4ba1561951de9ae25821fac9518baf2efa3
Desire Chukwudi <106650873+MrChukwudi@users.noreply.github.com>
2026-03-16 01:07:33 +0100

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,12 @@
+-- This particular script must be run in the postgress 'Master' database level:
+-- Kick everyone out
+SELECT pg_terminate_backend(pid)  -- The "Action" (Kill this process)
+FROM pg_stat_activity            -- The "Source" (The list of all active users)
+WHERE datname = 'deedatawarehouse' -- The "Filter" (Only people in MY database)
+  AND pid <> pg_backend_pid();   -- The "Safety" (Don't kill MY current connection)
+
+-- Drop and Rebuild
+DROP DATABASE IF EXISTS deedatawarehouse;
+CREATE DATABASE deedatawarehouse;
+
 
